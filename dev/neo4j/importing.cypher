97b657e76392741f11516bb9f0b489c79e07d254
Egor Dmitriev <egordmitriev2@gmail.com>
2022-04-29 09:49:32 +0200

    feat: Initial commit

---
+++ dev/neo4j/importing.cypher
@@ -0,0 +1,15 @@
+MATCH (resource:Resource) DETACH DELETE resource;
+
+CALL n10s.graphconfig.init();
+
+CALL n10s.graphconfig.set({
+  keepLangTag: false
+});
+
+CREATE CONSTRAINT n10s_unique_uri ON (r:Resource) ASSERT r.uri IS UNIQUE
+
+CALL n10s.rdf.import.fetch(
+  "file:///home/egordm/.config/Neo4j Desktop/Application/relate-data/dbmss/dbms-972a219f-16da-4dac-886c-c9d9ab12a0c9/import/Law_2021.nt.gz",
+  "Turtle"
+);
+
---
+++ dev/stardog/commands.sh
@@ -0,0 +1,11 @@
+/opt/stardog/bin/stardog-admin db create -o search.enabled=true search.index.properties.included=http:\/\/www.w3.org\/2000\/01\/rdf-schema\#label strict.parsing=false index.statistics.chains.enabled=false -n wikidata_law /var/data/import/Law_2021.nt.gz
+/opt/stardog/bin/stardog-admin db create -o search.enabled=true search.index.properties.included=http:\/\/www.w3.org\/2000\/01\/rdf-schema\#label strict.parsing=false index.statistics.chains.enabled=false -n wikidata_law /var/data/import/latest-truthy.nt.bz2
+
+# https://docs.stardog.com/graph-analytics/setup#running-graph-analytics
+dc exec spark bin/spark-submit --master "spark://localhost:7077" /opt/bitnami/spark/jars/stardog-spark-connector-1.0.1.jar algorithm.name=PageRank stardog.server=http://stardog:5820 stardog.database=wikidata_law output.property=example:analytics:rank output.graph=example:analytics:graph
+
+dc exec spark bin/spark-submit --master "spark://6a65595502ea:7077" /opt/bitnami/spark/jars/stardog-spark-connector-1.0.1.jar algorithm.name=ConnectedComponents stardog.server=http://stardog:5820 stardog.database=wikidata_law output.property=example:analytics:cc output.graph=example:analytics:graph
+# NOTE: make sure stardog-graph-analytics exists
+
+
+stardog data export myDb export.ttl.gz
