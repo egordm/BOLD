42e35aa2fd17b728df87c085cc84c948f1ddce2c
Egor Dmitriev <egordmitriev2@gmail.com>
2022-06-17 09:28:58 +0200

    refactor: Moved notebooks namespace to reports

--- backend/notebook/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from data.models import Dataset
-
-
-class Notebook(models.Model):
-    name = models.CharField(max_length=255)
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
-    content = models.JSONField()
