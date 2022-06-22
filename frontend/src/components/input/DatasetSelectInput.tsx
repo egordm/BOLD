c221e4434f1e154e0fb3c7f40d4615e24d746537
Egor Dmitriev <egordmitriev2@gmail.com>
2022-06-23 01:18:08 +0200

    feat: Added dataset selection input

---
+++ frontend/src/components/input/DatasetSelectInput.tsx
@@ -0,0 +1,79 @@
+import { Autocomplete, Chip, Grid, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Dataset } from "../../types/datasets";
+import { useFetchList } from "../../utils/api";
+
+
+export const DatasetSelectInput = (props: {
+  onValueChange?: (value: Dataset[]) => void,
+}) => {
+  const {
+    onValueChange,
+    ...rest
+  } = props;
+
+  const [ value, setValueInternal ] = useState<Dataset[]>([]);
+  const [ options, setOptions ] = useState<readonly Dataset[]>([]);
+
+  const setValue = (value: Dataset[]) => {
+    setValueInternal(value);
+    onValueChange(value);
+  }
+
+
+  const { setQuery } = useFetchList<Dataset>('/datasets/', {}, {
+    onSuccess: results => {
+      let newOptions: readonly Dataset[] = [];
+
+      if (value) {
+        newOptions = [...value];
+      }
+
+      if (results) {
+        newOptions = [...newOptions, ...results];
+      }
+
+      setOptions(newOptions);
+    }
+  });
+
+  return (
+    <Autocomplete
+      {...rest}
+      getOptionLabel={(option) =>
+        typeof option === 'string' ? option : option.name
+      }
+      multiple
+      limitTags={1}
+      filterOptions={(x) => x}
+      options={options}
+      autoComplete
+      includeInputInList
+      filterSelectedOptions
+      value={value}
+      onChange={(event: any, newValue: Dataset[] | null) => {
+        if (newValue.length > 1) {
+          newValue = [newValue[newValue.length - 1]];
+        }
+
+        setValue([
+          ...newValue.filter((option) => options.find((candidate) => candidate.id == option.id)),
+        ]);
+      }}
+      onInputChange={(event, newInputValue) => {
+        setQuery(newInputValue);
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Dataset" fullWidth/>
+      )}
+      renderTags={(tagValue, getTagProps) =>
+        tagValue.map((option, index) => (
+          <Chip
+            label={option.name}
+            {...getTagProps({ index })}
+          />
+        ))
+      }
+    />
+  );
+}
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import React, { useState } from "react";
+import { useQuery, UseQueryOptions } from "react-query";
+import { Dataset } from "../types/datasets";
 
 
 export const apiClient = axios.create({
@@ -10,4 +13,68 @@ export interface PaginatedResult<T> {
   next: string | null;
   previous: string | null;
   results: T[];
+}
+
+export interface PaginationParams {
+  page: number;
+  limit: number;
+  query: string;
+  ordering: string;
+}
+
+export const apiFetchList = <T, >(
+  endpoint: string,
+  updateCountFn: (count: number) => void
+) => (
+  async (page = 0, limit = 20, query = '', ordering = '-created_at') => {
+    const params = {
+      offset: page * limit,
+      limit: limit,
+      ordering: ordering,
+    }
+
+    if (query) {
+      params['search'] = query;
+    }
+
+    const response = await apiClient.get<PaginatedResult<T>>(endpoint, { params })
+    updateCountFn(response.data.count);
+
+    return response.data.results;
+  }
+)
+
+export const useFetchList = <T, >(
+  endpoint: string,
+  pagination: Partial<PaginationParams>,
+  options: UseQueryOptions<T[]>,
+) => {
+  const [ count, setCount ] = useState(1);
+  const [ page, setPage ] = useState(pagination.page ?? 0);
+  const [ limit, setLimit ] = useState(pagination.limit ?? 20);
+  const [ query, setQuery ] = useState(pagination.query ?? "");
+  const [ ordering, setOrdering ] = useState(pagination.ordering ?? "-updated_at");
+
+  const fetchItems = apiFetchList<T>(endpoint, setCount);
+
+  const result = useQuery(
+    [ endpoint, page, limit, query, ordering ],
+    () => fetchItems(page, limit, query, ordering),
+    { ...options, keepPreviousData: true }
+  )
+
+  const refresh = () => {
+    setPage(0);
+    result.refetch();
+  }
+
+  return {
+    ...result,
+    count,
+    page, setPage,
+    limit, setLimit,
+    query, setQuery,
+    ordering, setOrdering,
+    refresh,
+  }
 }
