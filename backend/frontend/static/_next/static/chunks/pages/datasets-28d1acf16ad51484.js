f332f1f605bb87220432eaa680eb1f4822f5656f
Egor Dmitriev <egordmitriev2@gmail.com>
2022-07-11 18:08:56 +0200

    feature: Updated static frontend content

--- backend/frontend/static/_next/static/chunks/pages/datasets-28d1acf16ad51484.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[671],{18444:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/datasets",function(){return u(44112)}])}},function(n){n.O(0,[502,83,392,732,551,13,648,112,774,888,179],(function(){return _=18444,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ backend/frontend/static/_next/static/chunks/pages/index-8f3f861cd94effeb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{48312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(35970)}])},35970:function(n,u,_){"use strict";_.r(u);var t=_(44112);u.default=t.default}},function(n){n.O(0,[502,811,267,25,13,648,112,774,888,179],(function(){return u=48312,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
