ee746732f82b7c3795b954d0c141b754a8c8b426
Egor Dmitriev <egordmitriev2@gmail.com>
2022-08-01 13:07:49 +0200

    feature: Updated static website

--- backend/frontend/static/_next/static/chunks/pages/index-e7d71e65dcec23cf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{48312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(35970)}])},35970:function(n,u,_){"use strict";_.r(u);var t=_(44112);u.default=t.default}},function(n){n.O(0,[502,464,17,551,13,91,112,774,888,179],(function(){return u=48312,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
