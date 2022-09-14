4ea7871430905df7830af238b4f8f68255676368
Egor Dmitriev <egordmitriev2@gmail.com>
2022-09-14 14:34:44 +0200

    bugfix: Updated entrypoint url not to use static port

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -3,9 +3,13 @@ import { useState } from "react";
 import { useQuery, UseQueryOptions } from "react-query";
 
 // @ts-ignore
-export const API_ENDPOINT= `${window.location.protocol}//${window.location.hostname}:8000/api`;
+export const API_ENDPOINT= window.location.port
+  ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api`
+  : `${window.location.protocol}//${window.location.hostname}/api`;
 // @ts-ignore
-export const WS_ENDPOINT= `ws://${window.location.hostname}:8000`;
+export const WS_ENDPOINT= window.location.port
+  ? `ws://${window.location.hostname}:${window.location.port}`
+  : `ws://${window.location.hostname}`;
 
 export const apiClient = axios.create({
   baseURL: API_ENDPOINT,
