13aa557cab833e3240a3aeab40eb9f79ee65ebbd
Egor Dmitriev <egordmitriev2@gmail.com>
2022-09-15 22:07:35 +0200

    bugfix: Fixed asgi middleware import order

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,17 +9,16 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from users.middleware import JwtAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 django_asgi_app = get_asgi_application()
 
+from users.middleware import JwtAuthMiddlewareStack
 import reports.routing
 import tasks.routing
 
